f4bba24b8ae9d8ebd73ab6ff21456fc8e7c5f3f3
Mario Lazzari <mario.lazzari@gmail.com>
2021-12-26 22:33:57 +0100

    Aded list

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import List from "./components/List";
 export interface IPerson {
   name: string;
   age: number;
-  url: string;
+  img: string;
   note?: string;
 }
 
@@ -15,7 +15,17 @@ interface IState {
 
 function App() {
   const [people, setPeople] = useState<IState["people"]>([
-    { name: "Lebron James", age: 36, url: "" },
+    {
+      name: "LeBron James",
+      age: 35,
+      img: "https://cdn.nba.com/headshots/nba/latest/1040x760/2544.png",
+      note: "Allegeric to staying on the same team",
+    },
+    {
+      name: "Kobe Bryant",
+      age: 42,
+      img: "https://fullpresscoverage.com/wp-content/uploads/2020/01/101524695-457220551.jpg",
+    },
   ]);
 
   return (
--- src/components/List.tsx
+++ src/components/List.tsx
@@ -6,13 +6,22 @@ interface IListProps {
 }
 
 const List: FC<IListProps> = ({ people }) => {
-  return (
-    <div>
-      {people.map(person => (
-        <h2>{person.name}</h2>
-      ))}
-    </div>
-  );
+  const renderList = (): JSX.Element[] => {
+    return people.map(person => {
+      return (
+        <li className="List">
+          <div className="List-header">
+            <img className="List-img" src={person.img} alt={person.name} />
+            <h2>{person.name}</h2>
+          </div>
+          <p>{person.age} years old</p>
+          <p className="List-note">{person.note}</p>
+        </li>
+      );
+    });
+  };
+
+  return <ul>{renderList()}</ul>;
 };
 
 export default List;
